ec84855f3d48ab1b59dd300cc57566542e3a740c
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-03-23 18:15:29 -0700

    Started on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,25 @@ import React, { useState } from 'react';
 
 function Header() {
     return (
-        <>
-            header
-        </>
+        <header className='header'>
+            <div className='brand'>
+                <h1>Ryan Fernandez</h1>
+            </div>
+            <ul className='navigation'>
+                <li className='nav-item'>
+                    <a>About</a>
+                </li>
+                <li className='nav-item'>
+                    <a>Projects</a>
+                </li>
+                <li className='nav-item'>
+                    <a>Contact</a>
+                </li>
+                <li className='nav-item'>
+                    <a>Resume</a>
+                </li>
+            </ul>
+        </header>
     )
 }
 

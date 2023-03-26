1013820cdf948eea67ec6b212c54ac3afd87a9d1
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-03-25 20:41:19 -0700

    Added starter code for contact form. Need Styling and form submission handling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact() {
+    const [contactForm, setContactForm] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        try {
+            // submit from
+            console.log(contactForm);
+
+            // reset contactForm
+            setContactForm({ ...contactForm,
+                name: '',
+                email: '',
+                message: ''
+            });
+
+        } catch (err) {
+            console.error(err);
+        }
+        
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setContactForm({ ...contactForm, [name]: value });
+    };
 
     return (
-        <>
+        <div>
             <h1>Contact</h1>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et dolor sit amet felis volutpat hendrerit. Suspendisse sollicitudin mauris nec felis condimentum luctus. Duis metus ipsum, maximus a sagittis id, pulvinar eu risus. Mauris pharetra ante quis elit iaculis, a pretium mi eleifend. Maecenas id sodales mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed dui leo.
-
-Aenean lacus sem, venenatis non dapibus sit amet, hendrerit eget est. In hac habitasse platea dictumst. Aenean ultrices libero convallis enim blandit, non dignissim felis vestibulum. Aliquam volutpat, dui a varius faucibus, justo massa dictum orci, a tincidunt lacus orci at magna. Nulla vel quam eros. Integer efficitur elit vel dictum interdum. Aliquam sodales massa orci, at rhoncus urna ultricies et. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris quis eros ac est lacinia porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean velit magna, feugiat at dolor ac, dignissim auctor nisi. Nulla at viverra tortor. Nam dapibus fringilla turpis, sit amet ullamcorper leo sagittis et. Mauris maximus interdum interdum. Quisque eu dolor id urna faucibus eleifend vitae ac nisi. Fusce pulvinar mauris sed magna egestas, id aliquam leo malesuada.
-            </p>
-        </>
+            <p>Have a question or want to work together? Leave a message and I'll get back to you as soon as possible.</p>
+            <form className="contact-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={contactForm.name}
+                    name="name"
+                    className="contact-input"
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={contactForm.email}
+                    name="email"
+                    className="contact-input"
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type="text"
+                    placeholder="Message"
+                    value={contactForm.message}
+                    name="message"
+                    className="contact-input"
+                    onChange={handleChange}
+                ></input>
+                <button className="contact-submit">Submit</button>
+            </form>
+        </div>
     );
 }
 
